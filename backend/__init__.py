27d1cbe4940e1403e6903b517c59d3bbaddc1ffc
Leonardo Caloni Munduruca <leomunduruca@gmail.com>
2024-11-09 20:54:43 -0300

    init

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,7 @@
 from .funcionario import Funcionario
 from .passageiro import Passageiro
 from .routes import app
+import db
 from .db import get_funcionario_collection
 from .db import get_passageiro_collection
 from .db import get_passenger_photos
